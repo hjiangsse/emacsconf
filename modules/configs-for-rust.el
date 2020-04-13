a79a7852d79509360682082f1c604bac56bd4d30
hjiang <hjiang@sse.com.cn>
2020-04-13 11:15:56 +0800

    commit

--- modules/configs-for-rust.el
+++ modules/configs-for-rust.el
@@ -1,7 +1,7 @@
-(add-to-list 'load-path "/Users/hjiang/.emacs.d/elpa/rust-mode")
-(autoload 'rust-mode "rust-mode" nil t)
-
-(require 'rust-mode)
+(use-package cargo)
+(use-package rust-mode)
+(use-package racer)
+(use-package flycheck-rust)
 
 (add-hook 'rust-mode-hook
 		  (lambda ()
@@ -14,3 +14,20 @@
 
 (add-hook 'rust-mode-hook 'auto-complete-for-rust)
 
+;;;------------------------------cargo-minor-mode-------------------------------
+(add-hook 'rust-mode-hook 'cargo-minor-mode)
+
+;;;-------------------------------rustfmt setting-------------------------------
+(setq rust-format-on-save t)
+
+;;;------------------------------------racer------------------------------------
+(setq racer-cmd "~/.cargo/bin/racer")
+(setq recer-rust-src-path "~/Code/rust/src")
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+
+;;;--------------------------------flycheck-rust--------------------------------
+(add-hook 'rust-mode-hook #'flycheck-mode)
+(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
