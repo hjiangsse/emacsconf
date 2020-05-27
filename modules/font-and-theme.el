1ae393779f39de3bb0c790a455d6b5afd84917d0
hjiang <hjiang@sse.com.cn>
2020-05-27 09:43:29 +0800

    commit

--- modules/font-and-theme.el
+++ modules/font-and-theme.el
@@ -1,1 +1,51 @@
+;;;-----------------------------load zenburn theme------------------------------
+(defun load-zen-burn-theme ()
+  "Load zen burn theme for emacs"
+  (interactive)
+  (use-package zenburn-theme)
+  (load-theme 'zenburn t))
+
+;;;-----------------------load solarized theme for emacs------------------------
+(defun load-zen-solarized-theme-light ()
+  "Load zen burn theme for emacs"
+  (interactive)
+  (use-package solarized-theme)
+  (load-theme 'solarized-light t))
+
+(defun load-zen-solarized-theme-dark ()
+  "Load zen burn theme for emacs"
+  (interactive)
+  (use-package solarized-theme)
+  (load-theme 'solarized-dark t))
+
+;;;------------------------load synityinc tomorrow theme------------------------
+(defun load-sanityinc-tomorrow-day ()
+  "Load zen burn theme for emacs"
+  (interactive)
+  (use-package color-theme-sanityinc-tomorrow)
+  (load-theme 'sanityinc-tomorrow-day t))
+
+(defun load-sanityinc-tomorrow-night ()
+  "Load zen burn theme for emacs"
+  (interactive)
+  (use-package color-theme-sanityinc-tomorrow)
+  (load-theme 'sanityinc-tomorrow-night t))
+
+(defun load-sanityinc-tomorrow-blue ()
+  "Load zen burn theme for emacs"
+  (interactive)
+  (use-package color-theme-sanityinc-tomorrow)
+  (load-theme 'sanityinc-tomorrow-blue t))
+
+(defun load-sanityinc-tomorrow-bright ()
+  "Load zen burn theme for emacs"
+  (interactive)
+  (use-package color-theme-sanityinc-tomorrow)
+  (load-theme 'sanityinc-tomorrow-bright t))
+
+(defun load-sanityinc-tomorrow-eighties ()
+  "Load zen burn theme for emacs"
+  (interactive)
+  (use-package color-theme-sanityinc-tomorrow)
+  (load-theme 'sanityinc-tomorrow-eighties t))
 
